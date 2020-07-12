10ba54b3d63473a550a67937d6371a8b4c112088
tkrwtnb <wtnbtkr0000@gmail.com>
2020-07-12 12:12:44 +0900

    first commit

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+// for use project root directory.
+
+void main() {
+  var currentDir = Directory.current;
+  var assetDirName = "assets";
+
+  List<String> list = List<String>();
+  currentDir
+      .list(recursive: true, followLinks: false)
+      .listen((FileSystemEntity entity) {
+    list.add(entity.path);
+
+    String p = entity.path;
+
+    if (p.indexOf(assetDirName) != -1) {
+      p = p.substring(p.indexOf(assetDirName) - 1);
+
+      if (!p.contains(".")) {
+        if (Platform.isWindows) p = p.replaceAll("\\", "/");
+        p = p.replaceFirst("/", "");
+        print("- " + p + "/");
+      }
+    }
+  });
+}
